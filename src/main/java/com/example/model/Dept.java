2fc4dfbdbc4ff8bd05facad8c10e70bcdec691e4
java <java@bigdata>
2017-09-08 14:37:59 +0900

    frist commit

---
+++ src/main/java/com/example/model/Dept.java
@@ -0,0 +1,25 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class Dept {
+
+	public static void main(String[] args) {
+		
+		int deptno;
+		String dname;
+		String loc;
+	}
+
+}
